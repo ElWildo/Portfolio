65518e8da5f931080af3f9f0e1c31bcec16ac6fb
elWildo <francescowild90.com>
2024-12-16 15:49:18 +0000

    Update Navigation

--- src/components/ReactComponents/HamburgerIcon.tsx
+++ src/components/ReactComponents/HamburgerIcon.tsx
@@ -1,12 +1,13 @@
+import classes from "./HamburgerIcon.module.scss";
 export default function HamburgerIcon() {
   const handleToggleClick = () => {
     document.querySelector(".nav-links")?.classList.toggle("expanded");
   };
   return (
-    <div class="hamburger" onClick={handleToggleClick}>
-      <span class="line"></span>
-      <span class="line"></span>
-      <span class="line"></span>
+    <div class={classes.hamburger} onClick={handleToggleClick}>
+      <span class={classes.line}></span>
+      <span class={classes.line}></span>
+      <span class={classes.line}></span>
     </div>
   );
 }
